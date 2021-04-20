81f2d40ac42dc77660fec50c188c1cd975ef09eb
SilMoolhuijsen <>
2021-04-20 20:05:50 +0000

    coded option to choose text speed

--- main.py
+++ main.py
@@ -1,27 +1,34 @@
-TextSpeed = input("Enter your preferred text speed (slow, normal or fast): ")
-while TextSpeed <= 3 and TextSpeed > 0:
-  if TextSpeed = "slow":
-    txtspeed = 0.55
-  elif TextSpeed = "normal":
-    txtspeed = 0.4
-  elif TextSpeed = "fast":
-    txtspeed = 0.25
-else:
-print("This text speed is not valid, please enter fast, normal or slow\n")
- 
-import sys
 import time
-def slowprint(s):
-  for letter in s:
-    sys.stdout.write(letter)
-    sys.stdout.flush()
-    time.sleep(txtspeed)
-
-slowprint("What's your name?\n")
-user_name = input()
-slowprint("Okay " + user_name)
-time.sleep(0.2)
-slowprint(", let's embark on an adventure!\n")
-time.sleep(1)
 import os
-os.system('cls' if os.name == 'nt' else "printf '\033c'")
+import sys
+
+def ChooseTextSpeed():
+  def slowprint(s):
+    for letter in s:
+      sys.stdout.write(letter)
+      sys.stdout.flush()
+      time.sleep(txtSpeed)
+
+  TextSpeed = input("Choose a text speed, Slow (1), Normal (2) or Fast (3): ")  
+  if TextSpeed in "Fast" or "fast" or "3":
+    txtSpeed = 0.025
+  elif TextSpeed in "Normal" or "normal" or "2":
+    txtSpeed = 0.075
+  elif TextSpeed in "Slow" or "slow" or "1":
+    txtSpeed = 0.0125
+  else:
+    print("That is not a valid text speed. Choose from slow, normal or fast.")
+    time.sleep(0.8)
+    ChooseTextSpeed()
+
+  def start():
+    slowprint("What's your name?\n")
+    user_name = input()
+    slowprint("Okay " + user_name)
+    time.sleep(0.2)
+    slowprint(", let's embark on an adventure!\n")
+    time.sleep(1)
+    os.system('cls' if os.name == 'nt' else "printf '\033c'")
+  start()
+
+ChooseTextSpeed()
